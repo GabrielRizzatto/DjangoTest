e269aeaf4d36f744bed21278fa40436be005bcbd
Gabriel Rizzatto <gabrielrizzatto3@gmail.com>
2024-07-06 18:52:36 -0300

    adicionado App home

--- project/urls.py
+++ project/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+
+def home(request):
+    print('home')
+    return HttpResponse('HOME')
+
+def my_view(request):
+    return HttpResponse('Uma mensagem')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', my_view),
+    path('', home),
 ]
